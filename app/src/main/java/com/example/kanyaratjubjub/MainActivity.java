571a2bc2de26e1ccb8dbdb70fe97d3eeb4be7e56
Pakrop <pakrop.t@psru.ac.th>
2025-06-26 16:46:08 +0700

    lab2

--- app/src/main/java/com/example/kanyaratjubjub/MainActivity.java
+++ app/src/main/java/com/example/kanyaratjubjub/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kanyaratjubjub;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        numMessage = 10;
+        Log.i("numMessage =", "" + numMessage);
+
+        numMessage++;
+        numMessage = numMessage + 1;
+        Log.i("numMessage =", "" + numMessage);
+
+
+        boolean isFriend = true;
+        Log.i("isFriend =", "" + isFriend);
+
+        String contact = "Kanyarat";
+        String message = "This is my first APP";
+        Toast.makeText(this, "message from" + contact, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "message is" + message, Toast.LENGTH_SHORT).show();
+        String command = "go east";
+        switch (command) {
+            case "go east":
+                Log.i("massage", "Player go to east");
+                break;
+            case "go west":
+                Log.i("massage", "Player go to west");
+                break;
+            default:
+                Log.i("massage", "Default message");
+                break;
+
+        }
+        Log.i("message:", "In countUp method");
+        int x = 0;
+        // Now an apparently infinite while Loob
+        while (true) {
+            x++; // Add 1 to x each time
+            Log.i("x =", "" + x); // print x
+            if (x == 3) {
+                break; // Get me out of here
+            }
+        }
+        String mess = joinThese("Method","are","cool");
+        Log.i("message=",mess);
+        float area = getAreaCircle(5f);
+        Log.i("area=",""+area);
+    }
+    String joinThese(String a, String b, String c) {
+        return a+b+c;
+    }
+    float getAreaCircle(float radius){
+        return 3.14f*radius;
     }
 }
